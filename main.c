2953d7b1cb833020eb5be62b65c822f876c9ceca
Reina <kdoro8540@gmail.com>
2020-11-24 20:01:28 -0500

    Update Shell Project

--- main.c
+++ main.c
@@ -67,3 +67,25 @@ void sigint_handler(int sigint)
 	write(STDOUT_FILENO, "\n$ ", 3);
 	fflush(stdout);
 }
+
+/**
+ * comparing - compare the built-in with the command entered and
+ * call the respective function to execute a command.
+ * @tokens: the tokenized line.
+ * Return: the built-in function result.
+ */
+int comparing(char **tokens)
+{
+	if (tokens[0] == NULL)
+		return (1);
+
+	if (_strcmp(tokens[0], "exit") == 0)
+		return (0);
+
+	if (_strcmp(tokens[0], "env") == 0)
+		_env(tokens);
+
+	if (_strcmp(tokens[0], "cd") == 0)
+		return (_cd(tokens));
+	return (1);
+}
