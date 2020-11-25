733fbf128020c16ee25f41cd3ade9a05002a19a0
Reina <kdoro8540@gmail.com>
2020-11-24 19:58:31 -0500

    Update Shell Project

--- main.c
+++ main.c
@@ -52,3 +52,18 @@ char *reading(void)
 	}
 	return (line);
 }
+
+/**
+* sigint_handler - Signal Handler function.
+* Prints a $ sign according to the command pressed.
+* @sigint: Reset handler to catch SIGINT next time.
+* Return: Nothing.
+*/
+void sigint_handler(int sigint)
+{
+	(void)sigint;
+
+	signal(SIGINT, sigint_handler);
+	write(STDOUT_FILENO, "\n$ ", 3);
+	fflush(stdout);
+}
