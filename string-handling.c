2d7c99a5686f4ba807df59cdfd35542c81287a89
Reina <kdoro8540@gmail.com>
2020-11-24 20:18:24 -0500

    Update - Shell project

--- string-handling.c
+++ string-handling.c
@@ -13,3 +13,29 @@ int _strlen(char *s)
 		i++;
 	return (i);
 }
+
+/**
+* _strcmp - compare two strings.
+* @s1: The pointer to the first string.
+* @s2: The pointer to the second string.
+* Return: return values that are as follows
+* if Return value < 0 then it indicates str1 is less than str2
+* if Return value > 0 then it indicates str2 is less than str1
+* if Return value = 0 then it indicates str1 is equal to str2.
+*/
+int _strcmp(char *s1, char *s2)
+{
+	char c1, c2;
+
+	do {
+		c1 = *s1++;
+		c2 = *s2++;
+
+		if (c1 == '\0')
+		{
+			return (c1 - c2);
+		}
+	} while (c1 == c2);
+
+	return (c1 - c2);
+}
