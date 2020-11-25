a77be5af151d6f1e12df7d8fe0721d7b1d4b52d0
Reina <kdoro8540@gmail.com>
2020-11-24 19:56:16 -0500

    Update Shell Project

--- main.c
+++ main.c
@@ -30,3 +30,25 @@ int main(void)
 	}
 	return (EXIT_SUCCESS);
 }
+
+/**
+* reading - reads an entire line from stdin, storing
+* the text including the newline and termination characters in a buffer
+* and storing the address of the buffer in line.
+* Return: The line read.
+*/
+char *reading(void)
+{
+	char *line = NULL;
+	size_t buffsize = 0;
+	ssize_t container = 0;
+
+	container = getline(&line, &buffsize, stdin);
+
+	if (container == -1)
+	{
+		free(line);
+		exit(0);
+	}
+	return (line);
+}
